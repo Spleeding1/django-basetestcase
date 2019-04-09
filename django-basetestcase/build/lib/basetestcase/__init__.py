efe654f9ad8cd41c2cf75d7c2916dd29d19a0796
Carl Brubaker <CrazyCarl@tgfdsunkh54fhjlkhgkfdhu.local>
2019-04-08 22:33:55 -0400

    still updating

--- django-basetestcase/build/lib/basetestcase/__init__.py
+++
@@ -1,8 +0,0 @@
-name = 'basetestcase'
-
-__version__ = '1.0.3'
-
-from .base_FormTestCase import *
-from .base_FunctionalTestCase import *
-from .base_ModelTestCase import *
-from .base_ViewTestCase import *
